b0fd57b148ba501d511cb3961f8ee051948b8894
Pine Mizune <pinemz@gmail.com>
2015-08-08 15:42:54 +0900

    Support Semaphore

---
+++ lib/Devel/Cover/Report/Codecov/Service/Semaphore.pm
@@ -0,0 +1,21 @@
+package Devel::Cover::Report::Codecov::Service::Semaphore;
+use strict;
+use warnings;
+use utf8;
+
+sub detect {
+    return $ENV{SEMAPHORE};
+}
+
+sub configuration {
+    return {
+        service => 'semaphore',
+        build   => $ENV{SEMAPHORE_BUILD_NUMBER},
+        commit  => $ENV{REVISION},
+        branch  => $ENV{BRANCH_NAME},
+        slug    => $ENV{SEMAPHORE_REPO_SLUG},
+    };
+}
+
+1;
+__END__
