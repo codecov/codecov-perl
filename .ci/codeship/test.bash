ce4dc2c9044db66c89e952bd89a46a0e558a0050
Pine Mizune <pinemz@gmail.com>
2015-08-15 15:08:47 +0900

    Delete CI files (Move to example-perl repo)

--- .ci/codeship/test.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-
-bash .ci/carton-exec-prove.bash
--- .ci/install.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-bash .ci/install-perl.bash
-bash .ci/install-cpanm.bash
-bash .ci/install-carton.bash
-bash .ci/carton-install.bash
