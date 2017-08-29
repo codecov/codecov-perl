dee2da666ba09fbcd91a7bf413dc0d5726229eeb
M Somerville <dracos@users.noreply.github.com>
2017-08-29 13:02:51 +0100

    Support uncoverable branches.
    This updates the reporting to say a branch is fully covered
    if all its parts are covered or marked as uncoverable.

--- lib/Devel/Cover/Report/Codecov.pm
+++ lib/Devel/Cover/Report/Codecov.pm
@@ -80,6 +80,8 @@ sub get_file_coverage {
 sub get_line_coverage {
     my ($statement, $branch) = @_;
 
+    # If all branches covered or uncoverable, report as all covered
+    return $branch->[0]->total.'/'.$branch->[0]->total if $branch && !$branch->[0]->error;
     return $branch->[0]->covered.'/'.$branch->[0]->total if $branch;
     return $statement unless $statement;
     return if $statement->[0]->uncoverable;
